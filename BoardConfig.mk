236abc619b472f271b927976228bcab8935bab85
alexandrusemina <alexandrusemina8@gmail.com>
2022-04-24 20:37:36 +0300

    a10: minor fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,3 @@ BOARD_VENDORIMAGE_PARTITION_SIZE   := 394264576
 
 # Inherit common board flags
 include device/samsung/universal7885-common/BoardConfigCommon.mk
-
-# System props
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
